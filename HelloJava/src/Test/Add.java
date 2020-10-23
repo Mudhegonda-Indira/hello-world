dd5ff6fee5ef78684b05e541740d481f67bf2bd2
Mudhegonda-Indira <indiramudhegonda2@gmail.com>
2020-10-23 08:19:34 +0530

    Added one more class

---
+++ HelloJava/src/Test/Add.java
@@ -0,0 +1,16 @@
+package Test;
+
+public class Add {
+
+	public static void main(String[] args) 
+	{
+		int a,b;
+		a=10;
+		b=10;
+		int c=a+b;
+		
+		System.out.println("Addition of two numbers is:  " +c);
+
+	}
+
+}
